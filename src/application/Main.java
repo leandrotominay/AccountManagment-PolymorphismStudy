9bcbb2203dc6605f92c61ea4fbfea74884fbcaa1
Leandro <ramosleandro53@gmail.com>
2020-12-10 13:57:27 -0300

    Adicionando explicações sobre UPCASTING e DOWNCASTING

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,13 +1,44 @@
 package application;
 
+import entities.Account;
 import entities.BusinessAccount;
+import entities.SavingsAccount;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        BusinessAccount account = new BusinessAccount(3301, "Leandro Ramos", 0.0, 500.0);
+        Account acc = new Account(1001, "Alex", 0.0);
+        BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.00);
 
-        System.out.println(account.getBalance());
+        // UPCASTING
+
+        Account acc1 = bacc;
+        acc1.getBalance();
+        Account acc2 = new BusinessAccount(1003, "Bob Joe", 0.0, 200.00);
+        Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
+
+        // DOWNCASTING
+
+        BusinessAccount acc4 = (BusinessAccount)acc2;
+        acc4.loan(100.0);
+
+        if (acc3 instanceof BusinessAccount){
+            BusinessAccount acc5 = (BusinessAccount)acc3;
+            acc5.loan(200.0);
+            System.out.println("Loan!");
+        }
+
+        if(acc3 instanceof SavingsAccount){
+            SavingsAccount acc5 = (SavingsAccount)acc3;
+            acc5.updateBalance();
+            System.out.println("Update!");
+        }
+
+
+
+
+
+
     }
 }
